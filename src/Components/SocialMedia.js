beeb837fb0407fe0d04a705e1bde35595aaa5735
Luis Méndez <mndzl@Luiss-MacBook-Air.local>
2022-05-13 10:43:12 -0400

    social media added

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -4,10 +4,10 @@ export default function SocialMedia(){
     return (
         <div className="social-container">
             <a href="https://www.instagram.com/mendezluis3/"><i className="social-i fa fa-instagram"></i></a>
-            <a href=""><i className="social-i fa fa-twitter"></i></a>
-            <a href="#"><i className="social-i fa fa-linkedin"></i></a>
-            <a href="#"><i className="social-i fa fa-github"></i></a>
-            <a href="#"><i className="social-i fa fa-envelope"></i></a>
+            <a href="https://twitter.com/Mendezzluis3"><i className="social-i fa fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/luisalejandromendez/"><i className="social-i fa fa-linkedin"></i></a>
+            <a href="https://github.com/mndzl"><i className="social-i fa fa-github"></i></a>
+            <a href="mailto:mendezgla.56@gmail.com"><i className="social-i fa fa-envelope"></i></a>
         </div>
     );
 }
